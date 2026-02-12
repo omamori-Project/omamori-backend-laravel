8b45e890ffce4f48c4ad99c693d26d9bea0a1084
jammmin02 <jmkm1201@naver.com>
2026-02-12 22:09:38 +0900

    feat(comment): 댓글 도메인 로직 및 권한 정책 추가

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,9 +5,11 @@ namespace App\Providers;
 use App\Models\Omamori;
 use App\Models\Share;
 use App\Models\Post;
+use App\Models\Comment;
 use App\Policies\OmamoriPolicy;
 use App\Policies\SharePolicy;
 use App\Policies\PostPolicy;
+use App\Policies\CommentPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class AuthServiceProvider extends ServiceProvider
         Omamori::class => OmamoriPolicy::class,
         Share::class   => SharePolicy::class,
         Post::class => PostPolicy::class,
+        Comment::class => CommentPolicy::class,
     ];
 
     public function boot(): void
